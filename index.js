e4f12e529fe5701e603472bc02209c3a41bd6d73
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-09-02 11:18:03 +0430

    add validations on schema

--- index.js
+++ index.js
@@ -9,11 +9,31 @@ mongoose
   .catch((err) => console.log("something went wrong: ", err));
 
 const courseSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 15,
+    enum: ["Node.js", "Angular.js", "React.js"],
+  },
   author: String,
-  tags: [String],
+  tags: {
+    type:Array,
+    validator:function(v){
+      return v && v.length > 0;
+    },
+    message:"a course should have at least one tag."
+  },
   date: { type: Date, default: Date.now() },
   isPublished: Boolean,
+  price: {
+    type: Number,
+    required: function () {
+      return this.isPublished;
+    },
+    min: 10,
+    max: 50,
+  },
 });
 
 // create a course model
@@ -22,16 +42,23 @@ const Course = mongoose.model("Course", courseSchema);
 async function createCourse() {
   // make an instance of Course model("constructor function")
   const course = new Course({
-    name: "Angular Course",
+    name: "React.js",
     author: "Mosh",
     tags: ["angular", "frontend"],
     isPublished: true,
+    price: 10,
   });
 
-  const result = await course.save(); // this returns the saved document in the collection, this operation is async
-  console.log(result);
+  try {
+    const result = await course.save(); // this returns the saved document in the collection, this operation is async
+    console.log(result);
+    // course.validate()
+  } catch (ex) {
+    console.log(ex.message);
+  }
 }
-// createCourse()
+
+createCourse();
 
 async function findCourse() {
   //   const courses = await Course.find({author:/Mosh/})
@@ -69,20 +96,17 @@ async function updateCourse(id) {
 
 // updateCourse("64f0828a514ed4bd09a3e027");
 
+async function deleteCourse(id) {
+  try {
+    // const course = await Course.deleteOne({_id:id})
+    // console.log(course)// return an acknowledge object
 
-async function deleteCourse(id){
-
-    try {
-        // const course = await Course.deleteOne({_id:id})
-        // console.log(course)// return an acknowledge object
-
-        // delete a document and get the deleted document 
-        const course = await Course.findOneAndRemove({_id:id})
-        // returns null if there is no document matching 
-        console.log(course)
-    }
-    catch (err) {
-        console.log(err)
-    }
+    // delete a document and get the deleted document
+    const course = await Course.findOneAndRemove({ _id: id });
+    // returns null if there is no document matching
+    console.log(course);
+  } catch (err) {
+    console.log(err);
+  }
 }
-deleteCourse("64f08292f68ddf968a887c08");
+// deleteCourse("64f08292f68ddf968a887c08");
